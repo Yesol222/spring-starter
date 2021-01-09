74d9f1ef6c751dacc871d161abeb49a77e003142
yesol <744348@gmail.com>
2021-01-09 23:13:01 +0900

    MVC

--- src/main/java/yesol/com/springstarter/controller/HelloController.java
+++ src/main/java/yesol/com/springstarter/controller/HelloController.java
@@ -1,9 +1,15 @@
-package controller;
+package yesol.com.springstarter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
-    
 
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!");
+        return "hello";
+    }
 }
